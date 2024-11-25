80a02f46ff313d7f544fdaba4c59911efbceee26
Gabbocnc <cianci.gabriele@hotmail.it>
2024-11-25 17:13:47 +0100

    add aboutUs route

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,21 +1,20 @@
+import Home from "./Home"
 export default function About() {
     return (
-        <p>
-            Benvenuti su MYBLOG.
-
-            Siamo un team di appassionati che condividono la nostra esperienza, conoscenza e curiosità con te attraverso articoli interessanti, utili e divertenti.
-
-            Il nostro obiettivo è fornire contenuti di alta qualità che ti aiutino a"pianificare il viaggio perfetto", "scegliere il dispositivo giusto per le tue esigenze",.
-
-            Ogni post è scritto con cura e attenzione, per offrirti informazioni pratiche, ispirazione e suggerimenti che possano arricchire la tua vita quotidiana.
-
-            In MyBlog, crediamo nel valore della comunità e nell'interazione con i nostri lettori. Siamo sempre felici di ricevere commenti, domande e suggerimenti, perché la tua opinione è fondamentale per crescere insieme.
-
-            Unisciti a noi in questo viaggio di apprendimento e scoperta. Che tu sia un esperto del settore o un principiante, troverai sempre qualcosa che ti ispira e ti aiuta a crescere.
-
-            Grazie per essere parte della nostra community!
-
-            Il team di MyBlog
-        </p>
+        <>
+            <Home />
+            <div className="container aboutUs">
+                <h2>Benvenuti su MYBLOG.</h2>
+                <article>
+                    <p>Siamo un team di appassionati che condividono la nostra esperienza, conoscenza e curiosità con te attraverso articoli interessanti, utili e divertenti.</p>
+                    <p>Il nostro obiettivo è fornire contenuti di alta qualità che ti aiutino a "pianificare il viaggio perfetto", "scegliere il dispositivo giusto per le tue esigenze".</p>
+                    <p>Ogni post è scritto con cura e attenzione, per offrirti informazioni pratiche, ispirazione e suggerimenti che possano arricchire la tua vita quotidiana.</p>
+                    <p>In MyBlog, crediamo nel valore della comunità e nell'interazione con i nostri lettori. Siamo sempre felici di ricevere commenti, domande e suggerimenti, perché la tua opinione è fondamentale per crescere insieme.</p>
+                    <p>Unisciti a noi in questo viaggio di apprendimento e scoperta. Che tu sia un esperto del settore o un principiante, troverai sempre qualcosa che ti ispira e ti aiuta a crescere.</p>
+                    <p>Grazie per essere parte della nostra community!</p>
+                    <p>Il team di MyBlog.</p>
+                </article>
+            </div>
+        </>
     )
 }
