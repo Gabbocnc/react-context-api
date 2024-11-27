933d398b23df23e75c80d3d12136d80ea2d06ceb
Gabbocnc <cianci.gabriele@hotmail.it>
2024-11-27 12:02:39 +0100

    add comments

--- src/App.jsx
+++ src/App.jsx
@@ -120,15 +120,16 @@ function App() {
         {/* rotte */}
 
         <Routes>
+          {/* rotta home page */}
           <Route path="/" element={<Home />} />
 
-
+          {/* rotta contatti */}
           <Route path="/contact" element={<Contact />} />
 
-
+          {/* rotta about */}
           <Route path="/about" element={<About />} />
 
-
+          {/* rotta per i blog  */}
           <Route path="/blog" element={
             <div className="dflex container bg-dark">
 
@@ -148,41 +149,15 @@ function App() {
                     setNewArticle={setNewArticle}
                   />
                 </div>
+                {/* rotta per tutti i Blog */}
                 <div className="col-12">
                   <BlogList filteredArticles={filteredArticles} fetchDeletePost={fetchDeletePost} />
-                  {/* <div className="row">
-                    {
-                      filteredArticles ?
-                        filteredArticles.map((data, index) => (
-                          <div className="col-6 p-3 position-relative" key={index}>
-                            <div className="card bg-secondary mb-3 text-white p-3">
-                              <h2 className="mb-2">{data.title}</h2>
-                              <div>{data.content}</div>
-                              <div className="mt-3 mb-3">
-                                <img src={data.image} style={{ maxWidth: 400, maxHeight: 300 }} alt="" />
-                              </div>
-                              <div className="tags">{data.tags}</div>
-                              <div className="status">
-                                <strong>Blog Status : </strong>{data.status}
-                              </div>
-                              <div className="position-absolute end-0 bottom-0 p-2">
-                                <button className="bg-danger" onClick={() => fetchDeletePost(data.slug)}>
-                                  <i className="bi bi-trash text-white"></i>
-                                </button>
-                                <Link className='btn btn-dark m-2' to={data.slug}>Go to Post</Link>
-                              </div>
-                            </div>
-                          </div>
-                        )) :
-                        <p>No results yet</p>
-                    }
-                  </div> */}
                 </div>
               </div>
             </div>
           } />
 
-
+          {/* rotta per visualizzare singolo blog */}
           <Route path="/blog/:slug" element={<BlogDetails />} />
 
 
